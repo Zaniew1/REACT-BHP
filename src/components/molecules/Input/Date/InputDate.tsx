84d9fdbceb8c9740f1c43f978c6b2ed6ca6aef50
Mateusz <m.zaniewski1995@gmail.com>
2025-02-22 12:12:41 +0100

    styles: add InputDate component and styles

--- src/components/molecules/Input/Date/InputDate.tsx
+++ src/components/molecules/Input/Date/InputDate.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Label } from '../../Label/Label';
+import Flatpickr from 'react-flatpickr';
+import 'flatpickr/dist/themes/dark.css';
+import { Polish } from 'flatpickr/dist/l10n/pl.js';
 
-export const InputDate = () => {
+type InputDateType = {
+  value: (value: number[]) => void;
+  id: string;
+  mode?: 'single' | 'multiple' | 'range';
+  default?: number[];
+  labelText?: string;
+  required?: boolean;
+  labelClass?: string;
+  wrapperClass?: string;
+  dateFormat?: string;
+  minDate?: string | Date;
+  maxDate?: string | Date;
+};
+export const InputDate = (props: InputDateType): React.JSX.Element => {
+  const handleInput = (date: Date[]) => {
+    const dates = date.map((el) => {
+      return new Date(el).getTime();
+    });
+    props.value(dates);
+  };
+
   return (
-    <div>InputDate</div>
-  )
-}
+    <Label
+      labelText={props.labelText}
+      labelClass={props.labelClass}
+      wrapperClass={props.wrapperClass}
+      id={props.id}
+      required={props.required}
+    >
+      <Flatpickr
+        id={props.id}
+        options={{
+          locale: Polish,
+          mode: props.mode ?? 'single',
+          defaultDate: props.default ?? [new Date()],
+          altInput: true,
+          altFormat: 'F j, Y',
+          dateFormat: 'Y-m-d',
+          minDate: props.minDate,
+          maxDate: props.maxDate,
+        }}
+        onChange={(date: Date[]) => {
+          handleInput(date);
+        }}
+      />
+    </Label>
+  );
+};
