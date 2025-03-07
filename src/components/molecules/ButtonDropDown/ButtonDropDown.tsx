69edefc1dfcb442e5bbf531d9a85f7aa3c307b8b
Mateusz <m.zaniewski1995@gmail.com>
2025-03-07 21:14:46 +0100

    chore: add dropDownButton

---
+++ src/components/molecules/ButtonDropDown/ButtonDropDown.tsx
@@ -0,0 +1,53 @@
+import { CSSProperties, SyntheticEvent, useEffect, useState } from 'react';
+import { Button } from '../../atoms/Button/Button';
+import { ChevronDown } from 'lucide-react';
+type ButtonDropDownType = {
+  id?: string;
+  buttonIcon?: React.ReactNode | string;
+  buttonText?: string;
+  buttonClass?: 'button--gray' | 'button--blue' | 'button--red' | 'button--none';
+  dropDownChildren?: React.ReactNode;
+  buttonStyle?: CSSProperties;
+  dropDownStyle?: CSSProperties;
+};
+
+export const ButtonDropDown = (props: ButtonDropDownType) => {
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const toggleDropdown = (e: SyntheticEvent) => {
+    e.stopPropagation();
+    setShowDropDown(!showDropDown);
+  };
+
+  const closeDropdown = () => {
+    setShowDropDown(false);
+  };
+  useEffect(() => {
+    if (showDropDown) {
+      document.addEventListener('click', closeDropdown);
+    } else {
+      document.removeEventListener('click', closeDropdown);
+    }
+
+    return () => document.removeEventListener('click', closeDropdown);
+  }, [showDropDown]);
+  const handleDropdownClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className={'dropdown'}>
+      <Button
+        class={props.buttonClass ?? 'button--gray'}
+        style={props.buttonStyle}
+        onClick={toggleDropdown}
+      >
+        {props.buttonIcon} {props.buttonText} {<ChevronDown size={18} />}
+      </Button>
+      {showDropDown && (
+        <div onClick={handleDropdownClick} style={props.dropDownStyle} className={'dropdown__area'}>
+          {props.dropDownChildren}
+        </div>
+      )}
+    </div>
+  );
+};
