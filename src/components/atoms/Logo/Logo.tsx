76e18dbef58ebbf580dcc23c22d620a2a0381e0e
Mateusz <m.zaniewski1995@gmail.com>
2025-03-16 13:54:20 +0100

    refactor: change path to img from absolute to relative

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 export const Logo = (): React.JSX.Element => {
   return (
     <a className="logo" href="/">
-      <img loading="eager" alt="Logo firmy" className="logo_icon" src="bhp.png" />
+      <img loading="eager" alt="Logo firmy" className="logo__icon" src="./../public/bhp.png" />
     </a>
   );
 };
